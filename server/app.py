bbeb803f257500508735870f2a6c1aebc6f4f263
anzar-hue <anzarkhan1697@gmail.com>
2026-04-10 17:01:49 +0530

    Fix server main entrypoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,12 @@
-from app import app
-
 def main():
+    import sys
+    import os
+
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+    from app import app
     app.run(host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
